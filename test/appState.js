ee66d1258fd9003a077f42ae1573692f5a142a19
Aditya Mukherjee <aditya@adityamukherjee.com>
2017-04-29 19:15:55 +0530

    set up for tests

---
+++ test/appState.js
@@ -0,0 +1,11 @@
+const assert = require("assert");
+
+describe("AppState", () => {
+
+  let App = require("../src/js/reducers/app");
+
+  it("should exist", () => {
+    assert(App !== undefined);
+  });
+
+});
